0d45c59496f0be189e45c57956b56fdc71c3ad70
Yuxin Sun <yuxisun@redhat.com>
2017-11-14 13:29:33 +0800

    Real-time output log

--- order_run.py
+++ order_run.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-from utils.utils_misc import *
+import subprocess
 import os
 import yaml
 
@@ -11,4 +11,13 @@ with open("{0}/cfg/cases_{1}.yaml".format(REALPATH, testsuite), 'r') as f:
     case_list_str = yaml.load(f).get("cases")
 cmd = "/usr/bin/avocado run {0}/tests/{1} --mux-yaml {2}/cfg/test.yaml".format(REALPATH, case_list_str, REALPATH)
 print(cmd)
-command(cmd)
+p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+while p.poll() is None:
+    line = p.stdout.readline()
+    line = line.strip()
+    if line:
+        print(line)
+if p.returncode == 0:
+    print("Run Success")
+else:
+    print("Run Failed")
