908f984da84e6f3c80344b7366b40dbcc951f9cf
Wei Shi <wshi@redhat.com>
2017-11-14 14:39:01 +0800

    Fix path issue in order_run.py and change execution order
    Change-Id: If1dd2e162047709d05c74db47b076ae7f568e75a

--- order_run.py
+++ order_run.py
@@ -9,7 +9,8 @@ REALPATH = os.path.split(os.path.realpath(__file__))[0]
 
 with open("{0}/cfg/cases_{1}.yaml".format(REALPATH, testsuite), 'r') as f:
     case_list_str = yaml.load(f).get("cases")
-cmd = "/usr/bin/avocado run {0}/tests/{1} --mux-yaml {2}/cfg/test.yaml".format(REALPATH, case_list_str, REALPATH)
+case_list = case_list_str.replace('testcase', REALPATH+'/tests/testcase')
+cmd = "/usr/bin/avocado run -d {0} --mux-yaml {1}/cfg/test.yaml --execution-order=tests-per-variant".format(case_list, REALPATH)
 print(cmd)
 p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 while p.poll() is None:
